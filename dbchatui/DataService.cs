8fc39f1b5bf12e0187209c14b5af29b774bb3205
Suryasmita Swain <suryasmita@asterissolutions.co.in>
2024-11-29 19:09:29 +0530

    Initial commit

---
+++ dbchatui/DataService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace YourOwnData
+{
+    public class DataService
+    {
+        private readonly string _connectionString;
+
+        public DataService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<List<string>> GetDataTable(string sqlQuery)
+        {
+            var rows = new List<List<string>>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        int count = 0;
+                        bool headersAdded = false;
+                        while (reader.Read())
+                        {
+                            count += 1;
+                            var cols = new List<string>();
+                            var headerCols = new List<string>();
+                            if (!headersAdded)
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    headerCols.Add(reader.GetName(i).ToString());
+                                }
+                                headersAdded = true;
+                                rows.Add(headerCols);
+                            }
+
+                            for (int i = 0; i <= reader.FieldCount - 1; i++)
+                            {
+                                try
+                                {
+                                    cols.Add(reader.GetValue(i).ToString());
+                                }
+                                catch
+                                {
+                                    cols.Add("DataTypeConversionError");
+                                }
+                            }
+                            rows.Add(cols);
+                        }
+                    }
+                }
+            }
+
+            return rows;
+        }
+    }
+
+    public class TableSchema()
+    {
+        public string TableName { get; set; }
+        public List<string> Columns { get; set; }
+    }
+}
